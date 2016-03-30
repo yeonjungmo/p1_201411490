d7b9dd326882d97c72ce8c0cd6ef71629dc41688
201411490_연정모 <wjdah7979@naver.com>
2016-03-30 14:21:24 +0900

    Added files via upload

---
+++ w3main.py
@@ -0,0 +1,27 @@
+﻿
+
+In [1]: import turtle
+
+In [2]: def Converttem():
+   ...:     cf=raw_input("Choose! c or f:")
+   ...:     if(cf=='c'):
+   ...:         print"you entered c,enter it`s value"
+   ...:         temp=raw_input("c value:")
+   ...:         ftemp=float(temp)
+   ...:         tem=(ftemp*1.8)+32
+   ...:         print tem
+   ...:     else:
+   ...:         print"you entered f,enter it`s value"
+   ...:         temp=raw_input("f value:")
+   ...:         ctemp=float(temp)
+   ...:         tem=(ctem-32)/1.8
+   ...:         print tem
+   ...:
+
+In [3]: Converttem()
+Choose! c or f:c
+you entered c,enter it`s value
+c value:110
+230.0
+
+In [4]:
