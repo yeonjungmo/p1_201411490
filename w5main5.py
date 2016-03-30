5ba99c795dc2cf2c27498466722f78172d0ead74
201411490_연정모 <wjdah7979@naver.com>
2016-03-30 15:42:12 +0900

    Added files via upload

---
+++ w5main5.py
@@ -0,0 +1,35 @@
+﻿In [1]: def TestMarks(marks):
+   ...:     if(90<=marks<=100):
+   ...:         grade='A'
+   ...:     elif(80<=marks<90):
+   ...:         grade='B'
+   ...:     elif(70<=marks<80):
+   ...:         grade='C'
+   ...:     elif(60<=marks<70):
+   ...:         grade='D'
+   ...:     elif(marks<60):
+   ...:         grade='F'
+   ...:     return grade
+   ...:
+
+
+
+In [2]: def jungmo():
+   ...:     entermarks=raw_input("enter your marks")
+   ...:     marks=float(entermarks)
+   ...:     mygrade=TestMarks(marks)
+   ...:     print mygrade
+   ...:
+
+
+
+
+In [3]: def main():
+   ...:     jungmo()
+   ...:
+
+
+In [4]:main()
+enter your marks60
+D
+
