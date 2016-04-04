05389b4943350130e76741824b8ed9b7f93bb495
201411490_연정모 <wjdah7979@naver.com>
2016-04-04 12:55:19 +0900

    Added files via upload

---
+++ w5RSP.py
@@ -0,0 +1,33 @@
+﻿user=raw_input("User Choice!(S=1,R=2,P=3):")   
+Computer=raw_input("Computer Choice!(S=1,R=2,P=3):")   
+  
+def Rsp():   
+      if user==1:   
+          if Computer==1:   
+              print "Same"   
+          if Computer==2:   
+              print "computer win"   
+          if Computer==3:   
+              print "user win"   
+      elif user==2:   
+          if Computer==1:   
+              print "user win"   
+          if Computer==2:   
+              print "Same"   
+          if Computer==3:   
+              print "computer win"   
+      elif user==3:   
+          if Computer==1:   
+              print computer win   
+          if Computer==2:  
+              print "user win"  
+          if Computer==3:   
+              print "Same"   
+
+
+def main():
+    Rsp()
+
+Rsp()
+
+wn.exitonclick()
