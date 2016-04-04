1b69674aeb5dbcb90ab609c2e9d91fe6ba687208
201411490_연정모 <wjdah7979@naver.com>
2016-04-05 08:40:11 +0900

    Added files via upload

---
+++ w5main(BMI).py
@@ -0,0 +1,19 @@
+﻿def ComputeBmi():
+   ...:     h=input ("user height:")
+   ...:     w=input ("user weight:")
+   ...:     bmi=w/(h*h)
+   ...:     if bmi<18.5:
+   ...:         result='low weight'
+   ...:     elif bmi>=18.5 and bmi<23:
+   ...:         result='normal weight'
+   ...:     elif bmi>=23 and bmi<25:
+   ...:         result='over weight'
+   ...:     elif bmi>=25 and bmi<30:
+   ...:         result='obesity'
+   ...:     elif bmi>=30:
+   ...:         result='try exercise'
+   ...:     return result
+   ...: 
+
+
+print ComputeBmi()
