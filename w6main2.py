9056282f99ce8e616b1d9c63dda3844dbe71c699
201411490_연정모 <wjdah7979@naver.com>
2016-04-11 13:56:21 +0900

    Added files via upload

---
+++ w6main2.py
@@ -0,0 +1,23 @@
+﻿
+y=list()
+sum=0
+for i in range(0,1001):
+    	if ((i%4==0) and (i%5 !=0)):
+       	x2.append(i)
+
+
+
+
+
+def SumList(alist):
+	sum =0
+ 	x2 = list()
+	for i in range(0,len(list)):
+     		sum = sum +list[i]
+              print sum
+def lab6:
+SumList(alist)
+
+def main():
+	def lab6
+   
