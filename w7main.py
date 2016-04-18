b223ae8c65fab67f67e6cdbab90593d9f3c7f980
201411490_연정모 <wjdah7979@naver.com>
2016-04-18 13:54:16 +0900

    Added files via upload

---
+++ w7main.py
@@ -0,0 +1,21 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.shape("turtle")
+def drawSquareAtSave(size,pos):
+    tracks=list()
+    t1.penup()
+    t1.setpos(pos,0)
+    t1.pendown()
+    for i in range(0,4):
+        t1.fd(100)
+        t1.right(90)
+        tracks.append(t1.pos())
+    print tracks
+def lab7():
+    mytracks=drawSquareAtSave(100,100)
+    print mytracks
+def main():
+    lab7()
+if __name__=="__main__":
+    main()
