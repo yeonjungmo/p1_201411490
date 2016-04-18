5be46b4a9887426fc8bb00e900ee2e21d5dacd0d
201411490_연정모 <wjdah7979@naver.com>
2016-04-18 22:49:01 +0900

    Update w7main.py

--- w7main.py
+++ w7main.py
@@ -2,6 +2,9 @@
 wn=turtle.Screen()
 t1=turtle.Turtle()
 t1.shape("turtle")
+x=dict()
+x=({0:(0,0),1:(0,100),2:(100,100),3:(100,0),4:(0,0)})
+
 def drawSquareAtSave(size,pos):
     tracks=list()
     t1.penup()
@@ -12,10 +15,19 @@ def drawSquareAtSave(size,pos):
         t1.right(90)
         tracks.append(t1.pos())
     print tracks
-def lab7():
+def drawSquareFront():
+    for j in range(1,5):
+        t1.goto(x[j])
+        
+def lab7_1():
     mytracks=drawSquareAtSave(100,100)
     print mytracks
+def lab7_2():
+    drawSquareFront()
+    
 def main():
-    lab7()
+    lab7_1()
+    lab7_2()
+    
 if __name__=="__main__":
     main()
