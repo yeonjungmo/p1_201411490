e301ca6c25a5d1182cdd7befcacc3668246e4c71
201411490_연정모 <wjdah7979@naver.com>
2016-05-16 13:49:04 +0900

    Add files via upload

---
+++ w11main2.py
@@ -0,0 +1,67 @@
+﻿import turtle
+wn=turtle.Screen()
+wn.bgpic("C:\Users\P400\Desktop\myMaze.gif")
+t1=turtle.Turtle()
+
+t1.shape("turtle")
+def namo():
+     t1.penup()
+     t1.goto(100,100)
+     t1.pendown()
+     t1.fd(100)
+     t1.left(90)
+     t1.fd(100)
+     t1.left(90)
+     t1.fd(100)
+     t1.left(90)
+     t1.fd(100)
+     t1.left(90)
+     t1.penup()
+     t1.home()
+     t1.pendown()
+
+     
+coord=[(100,100),(200,200)]
+curpos=(0,0)
+
+def isRectangle(curpos,coord):
+     return coord[0][1]<=curpos[1]<=coord[1][1] and coord[0][0] <= curpos[0]<=coord[1][0]
+     
+def keyup():
+     pt=t1.pos()
+    
+     t1.fd(50)
+     
+     isRectangle(pt,coord)
+ 
+
+def keydown():
+     pt=t1.pos()
+
+     t1.back(50)
+     isRectangle(pt,coord)
+
+
+def keyleft():
+     pt=t1.pos()
+
+     t1.left(90)
+     isRectangle(pt,coord)  
+
+def keyright(): 
+     pt=t1.pos()
+
+     t1.right(90)
+     isRectangle(pt,coord)
+
+def addkeys():
+     wn.onkey(keyup,"Up")
+     wn.onkey(keydown,"Down")
+     wn.onkey(keyleft,"Left")
+     wn.onkey(keyright,"Right")
+
+
+namo() 
+addkeys()
+wn.listen()
+turtle.mainloop()
---
+++ w11main3.py
@@ -0,0 +1,10 @@
+﻿import math
+def isinCircle(curpos,radius,pos):
+     t1.circle(radius)
+     pa=math.sqrt(math.pow(curpos[0]-pos[0],2)+math.pow(curpos[1]-pos[1],2))
+     if(pa>radius):
+         print true
+     else(pa<=radius):
+         print fales
+
+	
