5f94d475f03bb4ca31fb72bd4073be19a8eb46e3
201411490_연정모 <wjdah7979@naver.com>
2016-05-16 12:22:47 +0900

    Add files via upload

---
+++ w11main2.py
@@ -0,0 +1,32 @@
+﻿import turtle
+wn=turtle.Screen()
+wn.bgpic("C:\Users\P400\Desktop\myMaze.gif")
+t1=turtle.Turtle()
+
+t1.shape("turtle")
+
+def keyup():
+     t1.fd(50)
+ 
+
+def keydown():
+     t1.back(50)
+
+
+def keyleft():
+     t1.left(90)
+  
+
+def keyright():
+     t1.right(90)
+
+def addkeys():
+     wn.onkey(keyup,"Up")
+     wn.onkey(keydown,"Down")
+     wn.onkey(keyleft,"Left")
+     wn.onkey(keyright,"Right")
+
+
+addkeys()
+wn.listen()
+turtle.mainloop()
