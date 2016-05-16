6217837ef9a5a18adb9acfad8767cfe6b12e9c1e
201411490_연정모 <wjdah7979@naver.com>
2016-05-16 13:03:47 +0900

    Update w11main2.py

--- w11main2.py
+++ w11main2.py
@@ -1,13 +1,35 @@
-﻿import turtle
+import turtle
 wn=turtle.Screen()
 wn.bgpic("C:\Users\P400\Desktop\myMaze.gif")
 t1=turtle.Turtle()
 
 t1.shape("turtle")
+def namo():
+     t1.penup()
+     t1.goto(100,100)
+     t1.pendown()
+     t1.fd(100)
+     t1.left(90)
+     t1.fd(100)
+     t1.left(90)
+     t1.fd(100)
+     t1.left(90)
+     t1.fd(100)
+     t1.left(90)
+     t1.penup()
+     t1.home()
+     t1.pendown()
+     
+coord=[(100,100),(200,200)]
+curpos=t1.pos()
 
+def isRectangle(curpos,coord):
+     return coord[0][1]<=curpos[1]<=coord[1][1] and coord[0][0] <= curpos[0]<=coord[1][0]
+     
+     
 def keyup():
      t1.fd(50)
- 
+     
 
 def keydown():
      t1.back(50)
@@ -25,8 +47,12 @@ def addkeys():
      wn.onkey(keydown,"Down")
      wn.onkey(keyleft,"Left")
      wn.onkey(keyright,"Right")
+     
 
 
-addkeys()
-wn.listen()
-turtle.mainloop()
+def main():
+     namo() 
+     addkeys()
+     isRectangle(curpos,coord)
+     wn.listen()
+     turtle.mainloop()
