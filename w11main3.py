0db33c16c42902f6d19713df40e5086a3bb39679
201411490_연정모 <wjdah7979@naver.com>
2016-05-17 12:47:32 +0900

    Update w11main3.py

--- w11main3.py
+++ w11main3.py
@@ -1,10 +1,58 @@
-﻿import math
-def isinCircle(curpos,radius,pos):
-     t1.circle(radius)
-     pa=math.sqrt(math.pow(curpos[0]-pos[0],2)+math.pow(curpos[1]-pos[1],2))
-     if(pa>radius):
-         print true
-     else(pa<=radius):
-         print fales
+import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+t1.penup()
+t1.setpos(100,100)
+t1.pendown()
+for i in range(0,4):
+	t1.fd(100)
+	t1.left(90)
+t1.penup()
+t1.setpos(-100,100)
+t1.pendown()
+t1.right(180)
+for i in range(0,4):
+	t1.fd(100)
+	t1.right(90)
+t1.right(90)
+t1.penup()
+t1.goto(-100,-100)
+oldpos=t1.pos()
+coord=[(100,100),(200,200)]
+xs=coord[0][0]
+xe=coord[1][0]
+ys=coord[0][1]
+ye=coord[1][1]
 
+def r1():
+	t1.fd(30)
+	x=float()
+	y=float()
+	(x,y)=t1.pos()
+	if(xs<=x<=xe and ys<=y<=ye or -xe<=x<=-xs and ys<=y<=ye):
+		print ("turtle is in the point square")
+		t1.write("turtle is in the point area")
+		t1.goto(oldpos)
+	
+def r2():
+	t1.back(10)
+def r3():
+	t1.right(90)
+def r4():
+	t1.left(90)
+wn.onkey(r1,"Up")
+wn.onkey(r2,"Down")
+wn.onkey(r3,"Right")
+wn.onkey(r4,"Left")
+def mousetogo(x,y):
+	t1.setpos(x,y)
+	(x,y)=t1.pos()
+	if(xs<=x<=xe and ys<=y<=ye or -xe<=x<=-xs and ys<=y<=ye):
+		print ("turtle is in the point square")
+		t1.write("turtle is in the point area")
+		t1.goto(oldpos)
+def addMouse():
+	wn.onclick(mousetogo)
+addMouse()
+wn.listen()
 	
